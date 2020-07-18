c028cc5a0ae4762a2d516d0fa8c5837c9f209e01
Recruta <yangabrieldosanjos@gmail.com>
2020-07-18 19:14:47 -0300

    first blood

---
+++ Assets/PlayerCombat.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerCombat : MonoBehaviour
+{
+    public Animator animator;
+    public Transform attackPoint;
+    public float attackRange = 0.5f;
+    public LayerMask enemiesLayers;
+    
+     void OnDrawGizmosSelected(){
+        if(attackPoint == null){
+            return;
+        }
+        Gizmos.DrawWireSphere(attackPoint.position, attackRange);
+    }
+    void Attack(){
+
+        animator.SetTrigger("Attack");
+        Collider2D [] hitEnemies = Physics2D.OverlapCircleAll(attackPoint.position, attackRange, enemiesLayers);
+
+
+        foreach(Collider2D enemy in hitEnemies){
+            Debug.Log("We hit"+ enemy.name);
+        }
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Space)){
+            Attack();
+        }
+        
+    }
+}
